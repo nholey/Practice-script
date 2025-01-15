c448ab6e9d3d9f93237a3336d9bfb6ed668b5437
nholey <holeynikita17@gmail.com>
2025-01-15 18:10:31 +0530

    Practice script

---
+++ Project.java
@@ -0,0 +1,86 @@
+package Sample_project;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.github.dockerjava.api.model.Driver;
+
+public class Project {
+	 WebDriver driver;
+	
+	 
+ @BeforeTest
+ public void setup() {
+	 System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\EHRC\\Downloads\\chromedriver-win64 (2)\\chromedriver-win64\\chromedriver.exe");
+	 
+	  driver = new ChromeDriver();
+	 driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+	 driver.manage().window().maximize();
+}
+ 
+  @Test
+  public void Radio() {
+	  //driver.findElement(By.xpath("//input[@value='radio1']")).click();
+	  driver.findElement(By.xpath("//input[@value='radio2']")).click();
+	  //driver.findElement(By.xpath("//input[@value='radio3']")).click();
+  }
+  
+  @Test
+  public void Suggession() {
+	  driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Sample Test");
+  }
+  
+  @Test
+  public void dropdown() {
+	  WebElement dropdown = driver.findElement(By.id("dropdown-class-example"));
+	  Select sel = new Select(dropdown);
+		sel.selectByIndex(2);
+  }
+  
+  @Test
+  public void checkbox() {
+	  driver.findElement(By.id("checkBoxOption1")).click();
+	  driver.findElement(By.id("checkBoxOption2")).click();
+	  driver.findElement(By.id("checkBoxOption3")).click();
+  }
+  
+// @Test
+// public void window() {
+//	 driver.findElement(By.xpath("//button[@onclick='openWindow()']")).click();
+// }
+ 
+ @Test
+ public void Tab() {
+	 driver.findElement(By.xpath("//a[@class='btn-style class1 class2']")).click();
+	 Set<String> windowHandles = driver.getWindowHandles();
+	 List<String> tabs = new ArrayList<>(windowHandles);
+	 driver.switchTo().window(tabs.get(1));
+	 driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+	 driver.findElement(By.xpath("//a[@class='main-btn']")).click();
+	 driver.switchTo().window(tabs.get(0));
+ }
+ 
+ @Test
+ public void alert() throws InterruptedException {
+	 
+	 driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+	 driver.findElement(By.xpath("//input[@name='enter-name']")).sendKeys("Test");
+	 //driver.findElement(By.id("alertbtn")).click();
+	 Thread.sleep(10000);
+	 driver.findElement(By.id("confirmbtn")).click();
+ }
+     
+}
